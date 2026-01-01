7bc25aecb8c2a29e8348010646f9b25307b5aea4
rezvan80 <rbeheshtinezhad80@gmail.com>
2026-01-01 04:06:44 +0330

    Update evaluation.py

--- evaluation.py
+++ evaluation.py
@@ -7,195 +7,107 @@ LICENSE.md file in the root directory of this source tree.
 
 import numpy as np
 import torch
-
-MAX_EPISODE_LEN = 1000
-
-
-def create_vec_eval_episodes_fn(
-    vec_env,
-    eval_rtg,
-    state_dim,
-    act_dim,
-    state_mean,
-    state_std,
-    device,
-    use_mean=False,
-    reward_scale=0.001,
-):
-    def eval_episodes_fn(model):
-        target_return = [eval_rtg * reward_scale] * vec_env.num_envs
-        returns, lengths, _ = vec_evaluate_episode_rtg(
-            vec_env,
-            state_dim,
-            act_dim,
-            model,
-            max_ep_len=MAX_EPISODE_LEN,
-            reward_scale=reward_scale,
-            target_return=target_return,
-            mode="normal",
-            state_mean=state_mean,
-            state_std=state_std,
-            device=device,
-            use_mean=use_mean,
-        )
-        suffix = "_gm" if use_mean else ""
-        return {
-            f"evaluation/return_mean{suffix}": np.mean(returns),
-            f"evaluation/return_std{suffix}": np.std(returns),
-            f"evaluation/length_mean{suffix}": np.mean(lengths),
-            f"evaluation/length_std{suffix}": np.std(lengths),
-        }
-
-    return eval_episodes_fn
+import time
 
 
-@torch.no_grad()
-def vec_evaluate_episode_rtg(
-    vec_env,
-    state_dim,
-    act_dim,
-    model,
-    target_return: list,
-    max_ep_len=1000,
-    reward_scale=0.001,
-    state_mean=0.0,
-    state_std=1.0,
-    device="cuda",
-    mode="normal",
-    use_mean=False,
-):
-    assert len(target_return) == vec_env.num_envs
+class SequenceTrainer:
+    def __init__(
+        self,
+        model,
+        optimizer,
+        log_temperature_optimizer,
+        scheduler=None,
+        device="cuda",
+    ):
+        self.model = model
+        self.optimizer = optimizer
+        self.log_temperature_optimizer = log_temperature_optimizer
+        self.scheduler = scheduler
+        self.device = device
+        self.start_time = time.time()
 
-    model.eval()
-    model.to(device=device)
+    def train_iteration(
+        self,
+        loss_fn,
+        dataloader,
+    ):
 
-    state_mean = torch.from_numpy(state_mean).to(device=device)
-    state_std = torch.from_numpy(state_std).to(device=device)
+        losses, nlls, entropies = [], [], []
+        logs = dict()
+        train_start = time.time()
 
-    num_envs = vec_env.num_envs
-    state = vec_env.reset()
+        self.model.train()
+        for _, trajs in enumerate(dataloader):
+            loss, nll, entropy = self.train_step_stochastic(loss_fn, trajs)
+            losses.append(loss)
+            nlls.append(nll)
+            entropies.append(entropy)
 
-    # we keep all the histories on the device
-    # note that the latest action and reward will be "padding"
-    states = (
-        torch.from_numpy(state)
-        .reshape(num_envs, state_dim)
-        .to(device=device, dtype=torch.float32)
-    ).reshape(num_envs, -1, state_dim)
-    actions = torch.zeros(0, device=device, dtype=torch.float32)
-    rewards = torch.zeros(0, device=device, dtype=torch.float32)
+        logs["time/training"] = time.time() - train_start
+        logs["training/train_loss_mean"] = np.mean(losses)
+        logs["training/train_loss_std"] = np.std(losses)
+        logs["training/nll"] = nlls[-1]
+        logs["training/entropy"] = entropies[-1]
+        logs["training/temp_value"] = self.model.temperature().detach().cpu().item()
 
-    ep_return = target_return
-    target_return = torch.tensor(ep_return, device=device, dtype=torch.float32).reshape(
-        num_envs, -1, 1
-    )
-    timesteps = torch.tensor([0] * num_envs, device=device, dtype=torch.long).reshape(
-        num_envs, -1
-    )
+        return logs
 
-    # episode_return, episode_length = 0.0, 0
-    episode_return = np.zeros((num_envs, 1)).astype(float)
-    episode_length = np.full(num_envs, np.inf)
+    def train_step_stochastic(self, loss_fn, trajs):
+        (
+            states,
+            actions,
+            rewards,
+            dones,
+            rtg,
+            timesteps,
+            ordering,
+            padding_mask,
+        ) = trajs
 
-    unfinished = np.ones(num_envs).astype(bool)
-    for t in range(max_ep_len):
-        # add padding
-        actions = torch.cat(
-            [
-                actions,
-                torch.zeros((num_envs, act_dim), device=device).reshape(
-                    num_envs, -1, act_dim
-                ),
-            ],
-            dim=1,
-        )
-        rewards = torch.cat(
-            [
-                rewards,
-                torch.zeros((num_envs, 1), device=device).reshape(num_envs, -1, 1),
-            ],
-            dim=1,
+        states = states.to(self.device)
+        actions = actions.to(self.device)
+        rewards = rewards.to(self.device)
+        dones = dones.to(self.device)
+        rtg = rtg.to(self.device)
+        timesteps = timesteps.to(self.device)
+        ordering = ordering.to(self.device)
+        padding_mask = padding_mask.to(self.device)
+
+        action_target = torch.clone(actions)
+
+        _, action_preds, _ = self.model.forward(
+            states,
+            actions,
+            rewards,
+            rtg[:, :-1],
+            timesteps,
+            ordering,
+            padding_mask=padding_mask,
         )
 
-        state_pred, action_dist, reward_pred = model.get_predictions(
-            (states.to(dtype=torch.float32) - state_mean) / state_std,
-            actions.to(dtype=torch.float32),
-            rewards.to(dtype=torch.float32),
-            target_return.to(dtype=torch.float32),
-            timesteps.to(dtype=torch.long),
-            num_envs=num_envs,
+        loss, nll, entropy = loss_fn(
+            action_preds,  # a_hat_dist
+            action_target,
+            padding_mask,
+            self.model.temperature().detach(),  # no gradient taken here
         )
-        state_pred = state_pred.detach().cpu().numpy().reshape(num_envs, -1)
-        reward_pred = reward_pred.detach().cpu().numpy().reshape(num_envs)
+        self.optimizer.zero_grad()
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), 0.25)
+        self.optimizer.step()
 
-        # the return action is a SquashNormal distribution
-        action = action_dist.sample().reshape(num_envs, -1, act_dim)[:, -1]
-        if use_mean:
-            action = action_dist.mean.reshape(num_envs, -1, act_dim)[:, -1]
-        action = action.clamp(*model.action_range)
-
-        state, reward, done, _ = vec_env.step(action.detach().cpu().numpy())
-
-        # eval_env.step() will execute the action for all the sub-envs, for those where
-        # the episodes have terminated, the envs will be reset. Hence we use
-        # "unfinished" to track whether the first episode we roll out for each sub-env is
-        # finished. In contrast, "done" only relates to the current episode
-        episode_return[unfinished] += reward[unfinished].reshape(-1, 1)
-
-        actions[:, -1] = action
-        state = (
-            torch.from_numpy(state).to(device=device).reshape(num_envs, -1, state_dim)
+        self.log_temperature_optimizer.zero_grad()
+        temperature_loss = (
+            self.model.temperature() * (entropy - self.model.target_entropy).detach()
         )
-        states = torch.cat([states, state], dim=1)
-        reward = torch.from_numpy(reward).to(device=device).reshape(num_envs, 1)
-        rewards[:, -1] = reward
+        temperature_loss.backward()
+        self.log_temperature_optimizer.step()
 
-        if mode != "delayed":
-            pred_return = target_return[:, -1] - (reward * reward_scale)
-        else:
-            pred_return = target_return[:, -1]
-        target_return = torch.cat(
-            [target_return, pred_return.reshape(num_envs, -1, 1)], dim=1
+        if self.scheduler is not None:
+            self.scheduler.step()
+
+        return (
+            loss.detach().cpu().item(),
+            nll.detach().cpu().item(),
+            entropy.detach().cpu().item(),
         )
-
-        timesteps = torch.cat(
-            [
-                timesteps,
-                torch.ones((num_envs, 1), device=device, dtype=torch.long).reshape(
-                    num_envs, 1
-                )
-                * (t + 1),
-            ],
-            dim=1,
-        )
-
-        if t == max_ep_len - 1:
-            done = np.ones(done.shape).astype(bool)
-
-        if np.any(done):
-            ind = np.where(done)[0]
-            unfinished[ind] = False
-            episode_length[ind] = np.minimum(episode_length[ind], t + 1)
-
-        if not np.any(unfinished):
-            break
-
-
-    trajectories = []
-    for ii in range(num_envs):
-        ep_len = episode_length[ii].astype(int)
-        terminals = np.zeros(ep_len)
-        terminals[-1] = 1
-        traj = {
-            "observations": states[ii].detach().cpu().numpy()[:ep_len],
-            "actions": actions[ii].detach().cpu().numpy()[:ep_len],
-            "rewards": rewards[ii].detach().cpu().numpy()[:ep_len],
-            "terminals": terminals,
-        }
-        trajectories.append(traj)
-
-    return (
-        episode_return.reshape(num_envs),
-        episode_length.reshape(num_envs),
-        trajectories,
-    )
